11dcd1cdbf4fa10c584de0e0ed46ae03815cabe5
Stefania F <steff.20f@gmail.com>
2021-10-16 18:36:29 -0300

    creamos los archivos

---
+++ js.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    const botonBuscarPorID= document.getElementById("btn1");
+    botonBuscarPorID.addEventListener("click", async function(){
+        let url= "";
+        let response= fetch(url);
+        if( response.ok){
+
+        }
+    })
+});
